76fee30c8a01e76cf272916b843b1f8deaed024c
Dildeep Gabrhi <dildeep.gabrhi@gmail.com>
2025-01-11 15:49:36 +0000

    404 fix2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/dilruba-website/',
+    base: process.env.NODE_ENV === 'production' ? '/dilruba-website/' : '/',
     server: {
         open: true,
     },
@@ -18,4 +18,4 @@ export default defineConfig({
             '@': '/src'
         }
     }
-})
+});
